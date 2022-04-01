5bae799bc51a0c7b2e52718745a387dbeb79e509
Mehul Kulshreshtha <kulmehul@users.noreply.github.com>
2022-04-01 22:01:40 +0530

    11-Nov-21

---
+++ darray.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+
+
+	int how;
+
+	cin>>how;
+
+	// array : some size : run time : dynamic array
+	// stack memory
+	int a[how];
+	cout<<sizeof(a)<<endl;
+
+	int more;
+	cin>>more;
+
+	a[more];
+	cout<<sizeof(a)<<endl;
+
+
+}
