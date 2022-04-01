61f3bc1156dcb0127542c2237a8dc9c58b275fe9
Mehul Kulshreshtha <kulmehul@users.noreply.github.com>
2022-04-01 22:03:37 +0530

    11-Nov-21

---
+++ func1.cpp
@@ -0,0 +1,30 @@
+#include "iostream"
+
+using namespace std;
+
+
+
+void lw()
+{
+	int a;
+	a=5;
+
+
+	cout<<a<<endl;
+
+}
+
+int main()
+{
+
+	lw();
+
+	int a;
+	a=99;
+	cout<<a<<endl;
+
+
+
+
+
+}
