2df0efdc31438a9427cded3b9928c23a08dc3216
Mehul Kulshreshtha <kulmehul@users.noreply.github.com>
2022-04-01 22:04:40 +0530

    12-Nov-21

---
+++ func2.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+
+using namespace std;
+
+
+void lw(int p, int q)
+{
+
+	int x = p;
+	int y = q;
+	int z;
+	z = x + y;
+//	cout<<z<<endl;
+	return z;
+	
+}
+int main()
+{
+
+	int i,j;
+	i = 5;
+	j = 10;
+
+	lw(i, j);
+}
